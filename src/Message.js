d82e3a1146f0ffd89c2213ae23fa7ff061a96511
svshilna <88377516+svshilna@users.noreply.github.com>
2025-06-23 14:18:48 +0400

    Update Message.js

--- src/Message.js
+++ src/Message.js
@@ -16,18 +16,18 @@ export default function Message() {
         {showGift ? "Hide Gift" : "Show Gift"}
       </button>
       {showGift && (
-<img
-  src="https://raw.githubusercontent.com/svshilna/birthday-gift/main/images/recliner.png"
-  alt="Recliner"
-  style={{
-    marginTop: 20,
-    maxWidth: '100%',
-    height: 'auto',
-    borderRadius: 10,
-    boxShadow: '0 4px 8px rgba(0,0,0,0.2)'
-  }}
-/>
-
+        <img
+          src="https://raw.githubusercontent.com/svshilna/birthday-gift/main/images/recliner.png"
+          alt="Recliner"
+          style={{
+            marginTop: 20,
+            maxWidth: '100%',
+            height: 'auto',
+            borderRadius: 10,
+            boxShadow: '0 4px 8px rgba(0,0,0,0.2)'
+          }}
+        />
+      )}
     </div>
   );
 }
@@ -48,12 +48,5 @@ const styles = {
     borderRadius: 5,
     marginTop: 20,
     cursor: 'pointer'
-  },
-  image: {
-    marginTop: 20,
-    maxWidth: '100%',
-    height: 'auto',
-    borderRadius: 10,
-    boxShadow: '0 4px 8px rgba(0,0,0,0.2)'
   }
 };
